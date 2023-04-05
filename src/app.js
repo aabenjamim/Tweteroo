ae9e837d03ae2de4b83365ff6cf87ed46a1337ef
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-05 14:34:35 -0300

    rota post tweets sem sign-up

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ const app = express()
 app.use(cors())
 const PORT = 5000
 
+const tweets = []
 
+app.post("/tweets", (req, res)=>{
+    const tweet = req.body
+    tweets.push(tweet)
+    res.send('OK')
+})
+
+app.get("/tweets", (req, res)=>{
+    
+})
 
 
 app.listen(PORT, ()=>console.log(`O servidor está rodando na porta ${PORT}`))
