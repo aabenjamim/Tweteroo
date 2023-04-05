a366d0a8dec5a88796be4426f1d6579532599dd9
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-05 15:10:01 -0300

    versão com post sign-up

--- src/app.js
+++ src/app.js
@@ -2,19 +2,35 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 const PORT = 5000
 
+const signs = []
 const tweets = []
 
-app.post("/tweets", (req, res)=>{
-    const tweet = req.body
-    tweets.push(tweet)
+app.post("/sign-up", (req, res)=>{
+    const sign = req.body
+    signs.push(sign)
     res.send('OK')
 })
 
+app.post("/tweets", (req, res)=>{
+    const tweet = req.body
+    function adicionar(){
+        const username = tweet.username
+        if(signs.includes(username)){
+            tweets.push(tweet)
+            return 'OK'
+        } else{
+            return 'UNAUTHORIZED'
+        }
+    }
+    res.send(adicionar())
+})
+
 app.get("/tweets", (req, res)=>{
-    
+
 })
 
 
